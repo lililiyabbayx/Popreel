e8608fe4d47885eaf58483aa64701be4805b719d
lililiyabbayx <farhinulfatanikaa@gmail.com>
2025-01-29 10:52:27 +0600

    sign-in

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [ {
-        protocol: 'https',
-        hostname: 'img.clerk.com',
-        port: '',        
-      },],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+        port: "",
+      },
+    ],
   },
 };
 
